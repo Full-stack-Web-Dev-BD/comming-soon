f4ff74e682d13bb0a01e478b9cb8ac2ed1f04053
Alamin <alaminprogramerr@gmail.com>
2021-11-15 03:31:19 -0800

    update

--- src/components/Optin/Optin.js
+++ src/components/Optin/Optin.js
@@ -9,15 +9,15 @@ const Optin = () => {
         <button style={{ margin: '0 5px' }} >Sign up</button>
         <button style={{ margin: '0 5px' }} >Subscribe</button>
       </div>
-      <div className="mt-2">
+      <div className="mt-4">
         <div className="social-icons-container">
           <div className="social-icons">
             <ul style={{ margin: '0', padding: '0' }} class="d-flex ">
-              <a href="https://discord.gg/CT3YpGzH5v" class="ml-auto"><i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-discord"></i> </a>
-              <a href="https://www.youtube.com/channel/UCyvmQVMsho6BH6lXxUzxLkQ" class="ml-auto"> <i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-youtube"></i></a>
-              <a href="https://web.facebook.com/N-X-T-G-E-M-110573431436306" class="ml-auto"> <img src={meta} style={{ width: '18px', borderRadius: '3px', color: 'white', margin: '0 10px' }} /></a>
-              <a href="https://twitter.com/nxtgem_io" class="ml-auto"> <i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-twitter"></i></a>
-              <a href="https://t.me/nxtgem_io" class="ml-auto"><i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-telegram"></i> </a>
+              <a style={{ color: 'transparent' }} href="https://discord.gg/CT3YpGzH5v" class="ml-auto"><i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-discord"></i> </a>
+              <a style={{ color: 'transparent' }} href="https://www.youtube.com/channel/UCyvmQVMsho6BH6lXxUzxLkQ" class="ml-auto"> <i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-youtube"></i></a>
+              <a style={{ color: 'transparent' }} href="https://web.facebook.com/N-X-T-G-E-M-110573431436306" class="ml-auto"> <img src={meta} style={{ width: '18px', borderRadius: '3px', color: 'white', margin: '0 10px' }} /></a>
+              <a style={{ color: 'transparent' }} href="https://twitter.com/nxtgem_io" class="ml-auto"> <i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-twitter"></i></a>
+              <a style={{ color: 'transparent' }} href="https://t.me/nxtgem_io" class="ml-auto"><i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-telegram"></i> </a>
             </ul>
           </div>
         </div>
