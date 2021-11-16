21bf208167d8e5e0da8c44c3caaf8d1d6135ff0a
Alamin <alaminprogramerr@gmail.com>
2021-11-16 09:37:07 -0800

    working

--- src/components/Countdown/Timer.js
+++ src/components/Countdown/Timer.js
@@ -43,7 +43,7 @@ class CountDown extends Component {
     }
   }
   componentDidMount() {
-    this.deadline = new Date("Dec 06, 2021 21:00:00").getTime();
+    this.deadline = new Date("Dec 12, 2021 21:00:00").getTime();
 
     this.x = setInterval(this.count, 1000);
   }
--- src/components/Optin/Optin.js
+++ src/components/Optin/Optin.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import "./Optin.css";
+
 import meta from '../meta.png';
+import discord from '../discord.png';
+import telegram from '../telegram.png';
+import twitter from '../twitter.png';
+import youtube from '../youtube.png';
+import subscribe from '../subscribe.png';
+import login from '../login.png';
+import signup from '../signup.png';
+
+
 const Optin = () => {
   return (
     <div>
       <div className="optin">
-        <button style={{ margin: '0 5px' }}  >Login</button>
-        <button style={{ margin: '0 5px' }} >Sign up</button>
-        <button style={{ margin: '0 5px' }} >Subscribe</button>
+        <img src={signup} style={{ height: '40px', borderRadius: '12px', marginRight: '10px' }} />
+        <img src={login} style={{ height: '40px', borderRadius: '12px' }} />
       </div>
-      <div className="mt-4">
+      <div className="mt-4" style={{ marginTop: '20px' }}>
         <div className="social-icons-container">
           <div className="social-icons">
             <ul style={{ margin: '0', padding: '0' }} class="d-flex ">
-              <a style={{ color: 'transparent' }} href="https://discord.gg/CT3YpGzH5v" class="ml-auto"><i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-discord"></i> </a>
-              <a style={{ color: 'transparent' }} href="https://www.youtube.com/channel/UCyvmQVMsho6BH6lXxUzxLkQ" class="ml-auto"> <i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-youtube"></i></a>
-              <a style={{ color: 'transparent' }} href="https://web.facebook.com/N-X-T-G-E-M-110573431436306" class="ml-auto"> <img src={meta} style={{ width: '18px', borderRadius: '3px', color: 'white', margin: '0 10px' }} /></a>
-              <a style={{ color: 'transparent' }} href="https://twitter.com/nxtgem_io" class="ml-auto"> <i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-twitter"></i></a>
-              <a style={{ color: 'transparent' }} href="https://t.me/nxtgem_io" class="ml-auto"><i style={{ color: 'white', margin: '0 10px' }} class=" icon fab fa-telegram"></i> </a>
+              <a style={{ color: 'transparent' }} href="https://discord.gg/CT3YpGzH5v" class="ml-auto">  <img src={discord} style={{ width: '40px', borderRadius: '7px', color: 'white', margin: '0 10px' }} /> </a>
+              <a style={{ color: 'transparent' }} href="https://www.youtube.com/channel/UCyvmQVMsho6BH6lXxUzxLkQ" class="ml-auto">  <img src={youtube} style={{ width: '40px', borderRadius: '7px', color: 'white', margin: '0 10px' }} /> </a>
+              <a style={{ color: 'transparent' }} href="https://web.facebook.com/N-X-T-G-E-M-110573431436306" class="ml-auto"> <img src={meta} style={{ width: '40px', borderRadius: '7px', color: 'white', margin: '0 10px' }} /></a>
+              <a style={{ color: 'transparent' }} href="https://twitter.com/nxtgem_io" class="ml-auto"> <img src={twitter} style={{ width: '40px', borderRadius: '7px', color: 'white', margin: '0 10px' }} /> </a>
+              <a style={{ color: 'transparent' }} href="https://t.me/nxtgem_io" class="ml-auto">  <img src={telegram} style={{ width: '40px', borderRadius: '7px', color: 'white', margin: '0 10px' }} /> </a>
+              <a style={{ color: 'transparent' }} href="#" class="ml-auto">  <img src={subscribe} style={{ height: '40px', borderRadius: '12px', color: 'white', margin: '0 10px' }} /> </a>
             </ul>
           </div>
         </div>
